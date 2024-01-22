c93833a1572b4f8325edeb67901b7a86c2c55e91
nayem-upo <nayemislam0181@gmail.com>
2024-01-22 20:24:08 +0600

    base api ts error solved

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BaseQueryFn, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logOut, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: "http://localhost:5000/api/v1",
@@ -12,9 +13,34 @@ const baseQuery = fetchBaseQuery({
         return headers;
     }
 })
+const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs> = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if (result?.error?.status === 401) {
+        const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+            method: 'POST',
+            credentials: "include"
+        });
+        const data = await res.json();
+
+        if (data?.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(
+                setUser({
+                    user,
+                    token: data?.data?.accessToken,
+                })
+            );
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logOut());
+        }
+    }
+    return result;
+};
+
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: baseQuery,
+    baseQuery: baseQueryWithRefreshToken,
     endpoints: () => ({})
 })
